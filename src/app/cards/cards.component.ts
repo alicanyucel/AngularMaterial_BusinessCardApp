fa466334b785e086d4920eb7b358862aad5fb6bc
Ali Can Yücel <yucelalican30@gmail.com>
2023-05-10 18:31:23 +0300

    .

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.scss']
 })
-export class CardsComponent {
+export class CardsComponent implements OnInit {
+  cardItem = {
+    title: 'Full Stack Developer -Msc Software Engineer',
+    name: 'Ali Can Yücel',
+    phone: '0541 692 36 75',
+    email: 'yucelalican@hotmail.com',
+    address: 'Pursaklar/Ankara'
+  };
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
 }
