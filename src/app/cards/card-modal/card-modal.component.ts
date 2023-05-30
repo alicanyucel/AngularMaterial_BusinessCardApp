4b7016bdd877f429876d05e63cce59caadd84189
Ali Can Yücel <yucelalican30@gmail.com>
2023-05-30 12:28:57 +0300

    .

--- src/app/cards/card-modal/card-modal.component.ts
+++ src/app/cards/card-modal/card-modal.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-card-modal',
   templateUrl: './card-modal.component.html',
   styleUrls: ['./card-modal.component.scss']
 })
-export class CardModalComponent {
-
+export class CardModalComponent implements OnInit {
+  cardForm!: FormGroup
+  constructor(private fb: FormBuilder) {
+  }
+  ngOnInit(): void {
+    this.cardForm = this.fb.group({
+      name:['',Validators.required],
+      title: ['',Validators.required],
+      phone:  ['',Validators.required],
+      email:['',Validators.required],
+      address:['',Validators.required],
+    });
+  }
 }
--- src/app/cards/cards.module.ts
+++ src/app/cards/cards.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { CardsRoutingModule } from './cards-routing.module';
 import { CardsComponent } from './cards.component';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { CardItemComponent } from './card-item/card-item.component';
-import {MatDialogModule} from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 import { DialogModule } from '@angular/cdk/dialog';
-import { from, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { MatInputModule } from '@angular/material/input';
 import { CardModalComponent } from './card-modal/card-modal.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     CardsComponent,
@@ -22,9 +21,12 @@ import { FormsModule } from '@angular/forms';
     CardsRoutingModule,
     MatCardModule,
     MatButtonModule,
+    MatInputModule,
     DialogModule,
     FormsModule,
     MatDialogModule,
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class CardsModule { }
